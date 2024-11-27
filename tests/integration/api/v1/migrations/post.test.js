8acadd3f0b3d126188126dadf6ebb6debec00ca2
Diogo Righi <diogorighi@gmail.com>
2024-11-27 14:54:44 -0300

    adds `/api/v1/migrations` endpoint

---
+++ tests/integration/api/v1/migrations/post.test.js
@@ -0,0 +1,31 @@
+import database from "infra/database.js";
+
+beforeAll(cleanDatabase);
+
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
+
+test("POST to /api/v1/migrations should return 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+
+  expect(response.status).toBe(201);
+
+  const responseBody = await response.json();
+
+  expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
+
+  const responseAfter = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+
+  expect(responseAfter.status).toBe(200);
+
+  const responseBodyAfter = await responseAfter.json();
+
+  expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBodyAfter.length).toEqual(0);
+});
